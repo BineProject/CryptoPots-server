4e07f9551a309b453ce875739dc17d118ec84c2a
leavemyyard <zhukovpavel2001@gmail.com>
2020-10-13 17:52:56 +0300

    2

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ from database import DataSaver
 
 app = Flask(__name__)
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 saver = DataSaver()
 monitor = Monitor(saver)
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,14 @@
 from app import app, monitor
 from flask import jsonify
-from flask_cors import cross_origin
 import dataclasses
 
 
 @app.route("/activePots")
-@cross_origin()
 def activePots():
     return jsonify([dataclasses.asdict(item) for item in monitor.get_pots_data()])
 
 
 @app.route("/userPots/<string:user>")
-@cross_origin()
 def userPots(user: str):
     return jsonify(
         [dataclasses.asdict(item) for item in monitor.get_user_pots_data(user)]
