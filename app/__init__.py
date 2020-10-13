a9b54d87a0fd34c3bcbcf55d91cab66d1d3e5d2c
leavemyyard <zhukovpavel2001@gmail.com>
2020-10-13 17:46:24 +0300

    Enabled cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from contract_monitor import Monitor
 from database import DataSaver
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 saver = DataSaver()
 monitor = Monitor(saver)
 
